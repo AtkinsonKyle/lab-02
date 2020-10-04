b9cef99bc8abaea56bf95a3a50ab35922edea389
Klace Koch <koch.klace@gmail.com>
2020-10-03 18:10:25 -0700

    Feature 2 done

--- js/app.js
+++ js/app.js
@@ -1,48 +1,45 @@
 'use strict';
 
-function Pictures(picture) {
-  this.image_url = picture.image_url;
-  this.title = picture.title;
-  this.description = picture.description;
-  this.keyword = picture.keyword;
-  this.horns = picture.horns;
+let $template = $('#picture-temp');
+let $dropdown = $('#dropdown');
+let $container = $('.container')
+
+function Pictures(image, title, description, keyword, horns) {
+  this.image = image;
+  this.title = title;
+  this.description = description;
+  this.keyword = keyword;
+  this.horns = horns;
 }
 
-// Rendering Manually
-// Pictures.prototype.render = function() {
-//   $('main').append(`
-//     <div class=${this.name}>
-//       <h2>${this.name}</h2>
-//       <img src="${this.image_url}" />
-//       <p>${this.description}</p>
-//     </div>
-//   `);
-// };
+let keywordArray = [];
 
-Pictures.prototype.render = function () {
-  let $picDuplicate = $('.picture-temp').clone();
-  $('main').append($picDuplicate);
-  $picDuplicate.find('h2').text(this.name);
-  $picDuplicate.find('img').attr('src', this.image_url);
-  $picDuplicate.find('p').text(this.description);
-  $picDuplicate.removeClass('picture-temp');
-  $picDuplicate.attr('class', this.name);
-};
+$.ajax('./data/page-1.json').then(data => {
+  data.forEach(item => {
 
-Pictures.readJson = () => {
-  const ajaxSettings = {
-    method: 'get',
-    dataType: 'json'
-  };
+    let pic = new Pictures(item.image_url, item.title, item.description, item.keyword, item.horns);
+    let $picDuplicate = $template.clone();
+    $picDuplicate.attr('class', `${pic.keyword} item`);
+    $picDuplicate.find('h2').text(pic.title);
+    $picDuplicate.find('p').text(pic.description);
+    $picDuplicate.find('img').attr('src', pic.image);
+    $container.append($picDuplicate);
+    if (keywordArray.indexOf(pic.keyword)=== -1){
+      keywordArray.push(pic.keyword);
+      $dropdown.append(
+        $('<option></option>').text(pic.keyword)
+      );
+    }
+  });
 
-  $.ajax('../data/page-1.json', ajaxSettings)
-    .then(data => {
-      data.forEach(item => {
-        let pic = new Pictures(item);
-        console.log(pic);
-        pic.render();
-      });
-    });
-};
+});
 
-$(() => Pictures.readJson());
+$dropdown.change(function(){
+  let $photo = $('.item');
+  $photo.hide();
+  let val = $(this).val();
+  console.log(val);
+  let $showto = $('.' + val);
+  $showto.show();
+});
+
